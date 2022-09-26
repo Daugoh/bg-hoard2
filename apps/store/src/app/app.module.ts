22a4c3c48433e97d8c7b94636ead54e6537877a6
Daugoh <dtivoli@slb.com>
2022-09-26 13:48:47 +0200

    test

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -4,10 +4,19 @@ import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { UiSharedModule } from '@bg-hoard/store/ui-shared';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, MatCardModule, UiSharedModule],
+  imports: [BrowserModule, MatCardModule, RouterModule.forRoot([
+{
+path: 'game/:id', // <---- HERE
+loadChildren: () =>
+    import('@bg-hoard/store/feature-game-detail').then(
+            (m) => m.StoreFeatureGameDetailModule
+          )
+}
+  ]), UiSharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
