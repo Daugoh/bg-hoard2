a2efdc8a670a1c85a5a31fadaefa7aa9325bacc1
Daugoh <dtivoli@slb.com>
2022-09-27 10:35:24 +0200

    lab14

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -2,7 +2,12 @@ import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
 export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+  const libName = 'util-' + schema.name;
+  await libraryGenerator(tree, {
+    name: libName,
+    directory: schema.directory,
+    tags: `scope:${schema.directory}`
+  });
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
